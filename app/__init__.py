82b4be8451f9cfa4e06ac3630a24d4cd6c10bfaf
artagel <dan@syssec.biz>
2018-02-28 20:20:03 +0400

    Update nginx config Add proxyfix for flask.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from os import environ
 
 from flask import Flask
-from flask_login import login_required
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from werkzeug.contrib.fixers import ProxyFix
 
 csrf = CSRFProtect()
 app = Flask(__name__)
@@ -13,18 +13,12 @@ app.jinja_env.lstrip_blocks = True
 app.jinja_env.add_extension('jinja2.ext.do')
 app.threaded = True
 
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
 config_path = environ.get("CONF_PATH", "app.config.ProductionConf")
 app.config.from_object(config_path)
 csrf.init_app(app)
 db = SQLAlchemy(app)
-
-
-# Views
-@app.route('/test')
-@login_required
-def home():
-    return 'Here you go!'
-
 
 from app import models
 from app import auth
